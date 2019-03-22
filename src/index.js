cd5c76f38d3e9d908f63cdf59728f8883770065d
Trevor Nagaba <trevornagaba@gmail.com>
2019-03-22 06:26:28 +0000

    Correct xisnext typo

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class Board extends React.Component {
     squares[i] = this.state.xIsNext? 'X': 'O';
     this.setState({
       squares: squares,
-      xIsNext: !this.state.isNext,
+      xIsNext: !this.state.xIsNext,
     });
   }
 
