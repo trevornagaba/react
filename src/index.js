eaca3b96c077f3c7e710d1786d1fd6639ec0f110
Trevor Nagaba <trevornagaba@gmail.com>
2019-03-22 05:21:44 +0000

    Add state and state change onclick

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button 
+        className="square"
+        onClick = {() => this.setState({value: 'X'})}
+        >
+        {this.state.value}
       </button>
     );
   }
@@ -13,7 +22,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={1} />;
+    return <Square value={i} />;
   }
 
   render() {
