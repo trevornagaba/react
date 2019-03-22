198f2a2c52d8e168ff523cefddff664f181c7252
Trevor Nagaba <trevornagaba@gmail.com>
2019-03-22 06:01:45 +0000

    Move states, control to board

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+function Square(props) {
   render() {
     return (
       <button 
         className="square"
-        onClick = {() => this.setState({value: 'X'})}
-        >
-        {this.state.value}
+        onClick = {() => this.props.onClick({value: 'X'})}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i) {
+    return (
+      <Square 
+      value = {this.state.squares[i]}
+      onClick = {() => this.handleClick(i)}
+      />
+    );
+  }
+  
   render() {
     const status = 'Next player: X';
 
