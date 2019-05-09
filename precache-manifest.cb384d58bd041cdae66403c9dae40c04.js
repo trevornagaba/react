a7623eeb2d1cfa196e3eba246e3f19837471db7a
Trevor Nagaba <trevornagaba@gmail.com>
2019-05-09 09:23:08 +0000

    Updates

--- precache-manifest.cb384d58bd041cdae66403c9dae40c04.js
+++ precache-manifest.cb384d58bd041cdae66403c9dae40c04.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react/static/js/runtime~main.bb50a20d.js"
   },
   {
-    "revision": "b3b3819593974df1c421",
-    "url": "/react/static/js/main.9af96224.chunk.js"
+    "revision": "56df38afdb3ac016bb55",
+    "url": "/react/static/js/main.96b04f02.chunk.js"
   },
   {
     "revision": "b4d33b1ae8eb39de7433",
     "url": "/react/static/js/2.aa045389.chunk.js"
   },
   {
-    "revision": "b0695b830ae6074b2cae6621f6d584be",
+    "revision": "de0da109c9dc5fd8654a20f9009e8fb4",
     "url": "/react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.acb1629a79405b6d066a50522bbd5c2b.js"
+  "/react/precache-manifest.cb384d58bd041cdae66403c9dae40c04.js"
 );
 
 workbox.clientsClaim();
