3d76b58cbb88da83580fda2a77e3f86f5ff6377d
Trevor Nagaba <trevornagaba@gmail.com>
2019-05-09 08:01:48 +0000

    Rollback to board game

--- src/index.js
+++ src/index.js
@@ -1,27 +1,121 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
+/*
+Square is a function/controlled component and has no state therefore no need to define it as a class that extends React.Component
+onClick = {function() {this.setState{value: 'X'}}}
+*/
 
-export class MapContainer extends Component {
-  render() {
+function Square(props) {
     return (
-        <Map
-          google={this.props.google}
-          zoom={14}
-          style={mapStyles}
-          initialCenter={{
-            lat: -1.2884,
-            lng: 36.8233
-          }}
-        />
+      <button className="square" onClick = {props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext? 'X': 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square 
+      value = {this.state.squares[i]}
+      onClick = {() => this.handleClick(i)}
+      />
+    );
+  }
+  
+  render() {
+    const status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDYXogTmMXbKU1OvVff0wo8wzAk83o7Fc0'
-})(MapContainer);
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+
+// import React, { Component } from 'react';
+// import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+// const mapStyles = {
+//   width: '100%',
+//   height: '100%'
+// };
+
+// export class MapContainer extends Component {
+//   render() {
+//     return (
+//         <Map
+//           google={this.props.google}
+//           zoom={14}
+//           style={mapStyles}
+//           initialCenter={{
+//             lat: -1.2884,
+//             lng: 36.8233
+//           }}
+//         />
+//     );
+//   }
+// }
+
+// export default GoogleApiWrapper({
+//   apiKey: 'AIzaSyDYXogTmMXbKU1OvVff0wo8wzAk83o7Fc0'
+// })(MapContainer);
