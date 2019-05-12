c656903dd662e5bd864aad02830d98d6ddc6ca11
trevornagaba <trevornagaba@gmail.com>
2019-05-12 05:04:59 +0000

    Add on hover functionality

--- src/App.js
+++ src/App.js
@@ -4,44 +4,71 @@ import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import DriverLocation from './Map';
 
 export class MapContainer extends Component {
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {}
-  };
+    state = {
+        markerClicked: false,
+        showingInfoWindow: false,  //Hides or the shows the infoWindow
+        activeMarker: {},          //Shows the active marker upon click
+        selectedPlace: {}
+    };
 
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
+    onMarkerClick = (props, marker, e) => {
+        this.setState({
+            markerClicked: true
+        })
+    };
 
-  onClose = props => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
+    onMouseoverMarker = (props, marker, e) => {
+        if (!this.state.showingInfoWindow) {
+            this.setState({
+                selectedPlace: props,
+                activeMarker: marker,
+                showingInfoWindow: true
+            })
+        }
+    };
+
+    onMouseoutMarker = props => {
+        if (!this.state.markerClicked) {
+            if (this.state.showingInfoWindow) {
+                this.setState({
+                    showingInfoWindow: false,
+                    activeMarker: null
+                });
+            }
+        }
     }
-  };
 
-  render() {
-    return (
-      <DriverLocation centerAroundCurrentLocation google={this.props.google}>
-        <Marker onClick={this.onMarkerClick} name={'driver location'} />
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
-        >
-          <div>
-            <h4>{this.state.selectedPlace.name}</h4>
-          </div>
-        </InfoWindow>
-      </DriverLocation>
-    );
-  }
+    onClose = props => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                markerClicked: false,
+                showingInfoWindow: false,
+                activeMarker: null
+            });
+        }
+    };
+
+    render() {
+        return (
+        <DriverLocation centerAroundCurrentLocation google={this.props.google}>
+            <Marker
+                onMouseover={this.onMouseoverMarker}
+                onClick={this.onMarkerClick}
+                onMouseout={this.onMouseoutMarker}
+                name={'marker'}
+            />
+            <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
+            >
+            <div>
+                <h4>{this.state.selectedPlace.name}</h4>
+            </div>
+            </InfoWindow>
+        </DriverLocation>
+        );
+    }
 }
 
 export default GoogleApiWrapper({
